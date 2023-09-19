2bd9bdc2d6634e729bf1fee00117a6c21dd6eb05
dani980boo <daniel980boo@gmail.com>
2023-09-19 11:09:21 -0300

    19/07

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -18,6 +18,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
 // view engine setup
@@ -32,6 +33,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+const wiki = require("./wiki.js");
+app.use("/wiki", wiki);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
